bf29c4e25426ae4c050a37c5a00375516680356f
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2017-01-28 14:56:13 -0500

    Cleaned up masking

--- trackColor.py
+++ trackColor.py
@@ -1,14 +1,10 @@
-# USAGE
-# python ball_tracking.py --video ball_tracking_example.mp4
-# python ball_tracking.py
 
-# import the necessary packages
 from collections import deque
 import numpy as np
 import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
+# Set up Arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
     help="path to the (optional) video file")
@@ -16,56 +12,42 @@ ap.add_argument("-b", "--buffer", type=int, default=64,
     help="max buffer size")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+# Define HSV values for color (Red in this case)
+redLower = (0,75,75)
+redUpper = (5,255,255)
 pts = deque(maxlen=args["buffer"])
 
-# if a video path was not supplied, grab the reference
-# to the webcam
+#Get webcam
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
 
-# otherwise, grab a reference to the video file
-else:
-    camera = cv2.VideoCapture(args["video"])
-
-# keep looping
 while True:
-    # grab the current frame
+    # Current Frame
     (grabbed, frame) = camera.read()
-
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if args.get("video") and not grabbed:
-        break
 
     # resize the frame, blur it, and convert it to the HSV
     # color space
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    frame = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    #Mask red with small cleanups
+    mask = cv2.inRange(hsv, redLower, redUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
+    # Display views
     res = cv2.bitwise_and(frame,frame, mask= mask)
-    cv2.imshow('Mask',mask)
-    cv2.imshow('Res',res)
-    cv2.imshow("Frame", frame)
+    cv2.imshow('Mask, PRESS Q TO CLOSE',mask)
+    cv2.imshow('Res, PRESS Q TO CLOSE',res)
+    contours = []
+    cv2.findContours(mask, contours, 0, 0, 0)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the 'q' key is pressed, stop the loop
+    im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #Close all winows on q
     if key == ord("q"):
         break
 
-# cleanup the camera and close any open windows
+#Close camera
 camera.release()
 cv2.destroyAllWindows()
